3c679d538d2ca1222ce3c276741f079672eb726d
Umer Khokhar <umerkhokhar1001@gmail.com>
2026-02-03 12:47:10 +0500

    transition deleted

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "@/app/globals.css";
 import { Footer, Header } from "@/app/components";
 import { ThemeProvider } from "./components/ThemeProvider";
-import PageTransition from "./components/PageTransition";
 
 const clashGrotesk = localFont({
   src: [
@@ -64,7 +63,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Header />
-          <PageTransition>{children}</PageTransition>
+          {children}
           <Footer />
         </ThemeProvider>
       </body>
